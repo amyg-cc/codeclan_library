b381738d5a553bddc30e0bbd2909319c55385d64
amy gallacher <amy.gallacher@codeclan.com>
2021-08-01 14:09:45 +0100

    Adds Searching, Adding and Removing books.

--- codeclan_library.py
+++ codeclan_library.py
@@ -24,9 +24,9 @@ library = {
     ]
 }
 
-from art import *
-Art=text2art(f'{library["name"]}') # Return ascii text (default font) and default chr_ignore=True 
-print(Art)
+# from art import *
+# Art=text2art(f'{library["name"]}') # Return ascii text (default font) and default chr_ignore=True 
+# print(Art)
 
 option = ""
 while option != "q":
@@ -41,20 +41,43 @@ while option != "q":
 
     if option == "1":
         print("Listing all books...")
-        # TODO - List all books
-        print(f'The books we habe available are... {library["books"]}')
+        print(f'The books we habe available are: {library["books"]}')
 
     if option == "2":
         print("Searching for a book by title...")
-        # TODO - Search for a book by title
-
+        search = input("Input the Title of the book you're looking for: ")
+        book_exists = False
+        for book in library["books"]:
+            if search == book["title"]:
+                book_exists = True
+                search_result = book
+        if book_exists == True:
+            search_result_title = search_result["title"]
+            search_result_author = search_result["author"]
+            print(f"Yes, we have {search_result_title} by {search_result_author} in our library.")
+        else:
+            print(f"Sorry, we don't have {search} in our library.")
+        
     if option == "3":
         print("Adding a book...")
-        # TODO - Add a book
+        book_title = input("Enter the book title you wish to add: ")
+        book_author = input("Who is the author of that book?: ")
+        book_addition = {"title": book_title, "author": book_author}
+        library["books"].append(book_addition)
 
     if option == "4":
         print("Removing a book...")
-        # TODO - Remove a book
+        delete_request = input("What is the title of the book you wish to delete?: ")
+        delete_request_valid = False
+        for book in library["books"]:
+            if delete_request == book["title"]:
+                delete_request_valid = True
+                delete_selection = book 
+        if delete_request_valid == True:
+            library["books"].remove(delete_selection)
+            print(f"{delete_request} has been removed from the Library.")
+        else:
+            print(f"We couldn't find {delete_request} in our Library.")
 
     if option == "5":
         print("Updating a book...")
